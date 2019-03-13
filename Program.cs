4c660bade4f9abd2ba8129fc2799bd6a8a6695ab
Unknown <ciszekrm@vcu.edu>
2019-03-12 23:39:34 -0400

    File adds

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Hangfire;
+using Hangfire.Common;
+
+namespace SimpleScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0].ToString();
+            RecurringJob.AddOrUpdate(
+                () => System.Diagnostics.Process.Start(path),
+                Cron.Daily);
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
